c7debd4ac2cb799f578fb8ec8d7ad05fc757ff6b
New Ideas Unlimited <bradsipperley@icloud.com>
2026-09-03 17:30:53 -0700

    feat: expose Master native built-in

--- packages/core/src/builtins/index.ts
+++ packages/core/src/builtins/index.ts
@@ -11,3 +11,4 @@ export * from './torrent-galaxy/index.js';
 export * from './seadex/index.js';
 export * from './easynews-search/index.js';
 export * from './library/index.js';
+export * from './master-native/index.js';
